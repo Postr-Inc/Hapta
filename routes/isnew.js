84b675d0ce4ed629e1f433a8e39c5e2b62c8312e
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2023-11-20 10:28:32 -0800

    new method to check if user is new or not

---
+++ routes/isnew.js
@@ -0,0 +1,13 @@
+export default async function isNew(req, res){
+ let result = await global.pb.admins.client.collection('users').getOne(req.params.id).then((res)=>{
+    let created = new Date(res.created)
+    let now = new Date()
+    let diff = now - created
+    let days = diff / (1000 * 60 * 60 * 24)
+    let isNew = days < 7
+    return {error: false, isNew}
+  }).catch((err)=>{
+    return {error: true, message: err.message}
+  })
+  res.json(result)
+}
