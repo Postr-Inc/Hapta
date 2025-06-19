9a5fac8d4321e39ec175d84c6835c6a77048a0b3
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2025-06-18 20:34:16 -0500

    Match updated recommended config for backend

--- config.ts
+++ config.ts
@@ -1,22 +1,18 @@
  
-export default {
-    database:{
-        AdminEmail: "",
-        AdminPassword:"",
-        DatabaseURL: ""
-    },
+export default { 
     Server:{
         Port: 8080,
         Nodes: ["current"],
         threads: 4 
     },
     Security:{
-        Secret: ""
+        Secret: "bunjinkson@bongle"
     },
-    ratelimits:{
+    ratelimit:{
         Max: 10,
-        Duration: 60,
+        Duration: 60000,
         IP: true,
+        isEnabled: true,
         Message:"You have reached the maximum number of requests per minute"
     }
 }
