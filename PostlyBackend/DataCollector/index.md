dc040c2df1feb3df913b4ac9430ff8099bf7e9df
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2025-06-30 14:07:47 -0500

    Delete PostlyBackend directory

--- PostlyBackend/DataCollector/index.md
+++
@@ -1,3 +0,0 @@
-Data Collection Privacy Factors
-
-It is with greater concern that privacy from data collection is horrendous
--- PostlyBackend/RateLimiter/index.ts
+++
@@ -1,9 +0,0 @@
-import { config } from ".."
-export default class RateLimiter{
-    pool: Map<string, string>
-    constructor(){
-        console.log(config)
-        this.pool = new Map()
-        this.config
-    }
-}
