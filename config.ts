2d9b31f44e218f43a89b5b1a79740f6540a28105
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2025-07-06 10:20:52 -0500

    1.8.3
    Added node support

--- config.ts
+++ config.ts
@@ -1,18 +1,22 @@
+import crypto from "crypto";
  
 export default { 
-    Server:{
-        Port: 8080,
-        Nodes: ["current"],
-        threads: 4 
-    },
-    Security:{
-        Secret: crypto.randomUUID()
-    },
-    ratelimit:{
-        Max: 10,
-        Duration: 60000,
-        IP: true,
-        isEnabled: true,
-        Message:"You have reached the maximum number of requests per minute"
-    }
+  Server:{
+    Port: 8080,
+    Nodes: [], // `localhost:3025` 
+    NodeId:0,
+    MainNode: "ws://localhost:8080/ws/cache-sync",
+    threads: 4,
+    nodeEnabled: false
+  },
+  Security:{
+    Secret: crypto.randomUUID()
+  },
+  ratelimit:{
+    Max: 10,
+    Duration: 60000,
+    IP: true,
+    isEnabled: true,
+    Message:"You have reached the maximum number of requests per minute"
+  }
 }
